1cb0db09cff7efe448957abdafb07eb5b243dbb5
kravchuk-st <qkp@ukr.net>
2023-06-20 01:28:40 +0300

    feat: add cp conponent

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { getPath } from '../utils.js';
+import { fork } from 'child_process';
+
+const SCRIPT_PATH = getPath(import.meta.url, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(SCRIPT_PATH, args, {silent: true});
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
