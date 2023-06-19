211f8f695eb6d74074135654cfbb9a94135f77ac
kravchuk-st <qkp@ukr.net>
2023-06-19 18:28:18 +0300

    feat: add modules component

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,9 @@
 const parseArgs = () => {
-    const resalt = process.argv.reduce((acc, value, i, arr) => {
+    const resalt = process.argv.slice(2).reduce((acc, value, i, arr) => {
         if (value.startsWith('--')) acc.push(`${value.substring(2)} is ${arr[++i]}`);
         return acc;
     }, []).join(', ');
-    
+
     console.log(resalt);
 };
 
