cbff5123af3d126cf6c42a0ec0ebbf8a2ace3442
kravchuk-st <qkp@ukr.net>
2025-04-24 14:52:00 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    const res = process.argv.slice(2).reduce((acc, el, i, arr) => {
+        if (el.startsWith('--')) acc.push(`${el.substring(2)} is ${arr[i + 1]}`);
+        return acc;
+    }, []).join(', ');
+
+    console.log(res);
 };
 
 parseArgs();
