37e4f18621449d0710754eb49099291f89ebe3c9
kravchuk-st <qkp@ukr.net>
2025-04-24 20:55:29 +0300

    feat: add streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform, pipeline } from 'stream';
+
+const reversStr = (str) => `${str.split('').filter(el => el !== '\n').reverse().join('')}\n\n`;
+
 const transform = async () => {
-    // Write your code here 
+    const transformStrim = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, reversStr(chunk.toString()));
+        },
+    });
+
+    pipeline(process.stdin, transformStrim, process.stdout, (err) => console.error(err))
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'fs';
+import { getPath } from '../utils.js';
+
+const pathFile = getPath(import.meta.url, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(pathFile, {flags: 'a'});
+    process.stdin.pipe(stream);
 };
 
 await write();
