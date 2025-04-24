a4238009ef936ba2c44b75f5f03e2d4d7d5307e1
kravchuk-st <qkp@ukr.net>
2025-04-24 21:36:35 +0300

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+import { getPath } from '../utils.js';
+
+const pathFile = getPath(import.meta.url, 'files', 'fileToCompress.txt');
+const pathFileArchived = getPath(import.meta.url, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(pathFile, 'utf-8');
+    const outStream = createWriteStream(pathFileArchived, {flags: 'w'});
+    
+    await pipeline(readStream, createGzip(), outStream)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createUnzip } from 'zlib';
+import { getPath } from '../utils.js';
+
+const pathArchiv = getPath(import.meta.url, 'files', 'archive.gz');
+const pathFile = getPath(import.meta.url, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(pathArchiv);
+    const outStream = createWriteStream(pathFile);
+
+    await pipeline(readStream, createUnzip(), outStream);
 };
 
 await decompress();
